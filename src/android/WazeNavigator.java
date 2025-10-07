697ff39ca1dc4eff59a81b311c39e58549f170a6
Adam Fain <adam@rapidtrade.biz>
2025-10-07 09:24:05 +0200

    reverted android part

--- src/android/WazeNavigator.java
+++ src/android/WazeNavigator.java
@@ -2,7 +2,7 @@ package com.adaltojunior.cordova.plugin;
 
 
 import org.apache.cordova.CordovaPlugin;
-
+import android.content.ActivityNotFoundException;
 import org.apache.cordova.CallbackContext;
 
 import android.content.Intent;
@@ -20,14 +20,27 @@ import java.lang.String;
 
 public class WazeNavigator extends CordovaPlugin {
 
+    String CLASS_NAME = this.getClass().getName();
+
+    private void openIntent(String urlToIntent) {
+        Intent intent = new Intent( Intent.ACTION_VIEW, Uri.parse( urlToIntent ) );
+        this.cordova.getActivity().startActivity(intent);
+    }
+
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("navigateByWaze")) {
+            String fromLat = data.getString(0);
+            String fromLng = data.getString(1);
             String toLat = data.getString(2);
             String toLng = data.getString(3);
 
-            openUrlIntentByApplication(toLat, toLng);
+            try {
+                openIntent(getUrlIntentByApplication(fromLat, fromLng, toLat, toLng));
+            } catch (ActivityNotFoundException ex){
+                LOG.e(CLASS_NAME, ex.getMessage());
+            }
 
             return true;
 
@@ -38,11 +51,35 @@ public class WazeNavigator extends CordovaPlugin {
         }
     }
 
-    private void openUrlIntentByApplication(String myLatitude, String myLongitude) {
-        Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("geo:" + myLatitude  + "," + myLongitude + "?q=" + myLatitude  + "," + myLongitude + "(" + myLatitude + "," + myLongitude + ")"));
+    private String getUrlIntentByApplication(String fromLat, String fromLng, String toLat, String toLng) {
 
-        this.cordova.getActivity().startActivity(intent);
+        String url = "market://details?id=com.waze";
+
+        if (verifyAplicationIsInstalled("com.waze")) {
+            url = "waze://?ll=" + toLat + "," + toLng + "&navigate=yes";
+        } else if (verifyAplicationIsInstalled("com.google.android.apps.maps")) {
+            String fromPosition = fromLat + "," + fromLng;
+            String toPosition = toLat + "," + toLng;
+
+            url = "http://maps.google.com/maps?saddr=" + fromPosition + "&daddr=" + toPosition;
+        }
+
+        return url;
+    }
+
+    private Boolean verifyAplicationIsInstalled(String wazepackage) {
+
+        try {
+
+            PackageManager application = this.cordova.getActivity().getPackageManager();
+            PackageInfo info = application.getPackageInfo(wazepackage, PackageManager.GET_ACTIVITIES);
+
+            return info.packageName.equals(wazepackage);
+
+        } catch (PackageManager.NameNotFoundException e) {
+            LOG.e(CLASS_NAME, e.getMessage());
+            return false;
+        }
 
     }
 }
